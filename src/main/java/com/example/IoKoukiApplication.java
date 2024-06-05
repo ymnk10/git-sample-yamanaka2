5011d7439d3f35e6ca5596777c1dc8ae2eb73ad1
ymnk10 <kseisaimin10@gmail.com>
2024-06-05 10:53:32 +0900

    first commit

---
+++ src/main/java/com/example/IoKoukiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoKoukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoKoukiApplication.class, args);
+	}
+
+}
